9f24cd5d5c5fffa1b8b1a6515b7ad0ccf93b02db
Axel Hovorka <axelspeedy@gmail.com>
2016-04-10 20:57:01 -0700

    Added files via upload

---
+++ install.sh
@@ -0,0 +1,38 @@
+
+echo "Hello, What is your name [Type it in]"
+read name
+say Hello $name
+echo Hello, $name
+echo I am quite intelligent
+echo Do you want to play a game $name ? yes or no
+read ans
+#if [$ans === "yes"] 
+#then
+echo "Lets Go!" 
+echo "This is the...."
+sleep 1.5; echo "NUMBER THINKER!"
+say I am thinking of a number between 1 and 10, what is the number?
+echo "I am thinking of a number between 1 and 10, what is the number? [Type it in]"
+
+read helpfuf
+if [[ $helpfuf == $(( ( RANDOM % 10 )  + 1 )) ]];
+then 
+say "Good Job!"
+echo "You're Right"
+echo "Want to play again? [yes/no]"
+read playagain
+if [[ $playagain == yes ]]
+then 
+sleep 2;cd && cd Desktop && clear && ./replay.sh
+else
+exit
+fi
+else 
+
+
+echo "WRONG! Try again..."
+
+sleep 2;cd && cd Desktop && clear && ./replay.sh
+fi
+
+
---
+++ replay.sh
@@ -0,0 +1,14 @@
+say I am thinking of a number between 1 and 10, what is the number?
+echo "I am thinking of a number between 1 and 10, what is the number? [Type it in]"
+
+read helpfuf
+if [[ $helpfuf == $(( ( RANDOM % 10 )  + 1 )) ]];
+then 
+say "Good Job!"
+echo "You're Right"
+else 
+
+echo "WRONG! Try again..."
+
+sleep 2;cd && cd Desktop && ./replay.sh
+fi
