ffb63eba48df473083236f21774a6d93c5f799b0
Axel Hovorka <axelspeedy@gmail.com>
2017-06-12 14:16:37 +0000

    Create update.sh

--- update.sh
+++ update.sh
@@ -1,6 +1,7 @@
 echo "Updating..."
-mdkir recent-v
-cd recent-v
+echo "Type the directory path you would like this update installed to. eg '~/Desktop' or '/usr/share' Then press <ENTER>"
+read dir
+cd $dir
 git clone https://github.com/axelspeedy/number-thinker-project.git
 cd number-thinker-project
 sudo chmod u+x game.sh
@@ -8,7 +9,7 @@ sudo chmod u+x play.sh
 sudo chmod u+x update.sh
 clear
 echo "Update Done!"
-echo " Now move this current directory and delete everything else"
+echo "The latest version is installed in..."
 pwd
 sleep 1;
 echo " Now we will play the game..."
