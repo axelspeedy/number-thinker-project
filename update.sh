5cccdf79801962bd8d0afb4edf3a17a1bfeec61f
Axel Hovorka <axelspeedy@gmail.com>
2017-06-14 10:22:18 -0700

    Create update.sh

--- update.sh
+++ update.sh
@@ -1,6 +1,22 @@
 echo "Updating..."
-echo "Type the directory path you would like this update installed to. eg '~/Desktop' or '/usr/share' Then press <ENTER>"
+echo "Type the directory path you would like this update installed to. eg '~/Desktop' or '/usr/share' OR type '..' install in the same place. Then press <ENTER>"
 read dir
+if ($dir === ..)
+cd ..
+rmdir number-thinker-project
+git clone https://github.com/axelspeedy/number-thinker-project.git
+cd number-thinker-project
+sudo chmod u+x game.sh
+sudo chmod u+x play.sh
+sudo chmod u+x update.sh
+clear
+echo "Update Done!"
+echo "The latest version is installed in..."
+pwd
+sleep 1;
+echo " Now we will play the game..."
+./game.sh
+else
 cd $dir
 git clone https://github.com/axelspeedy/number-thinker-project.git
 cd number-thinker-project
@@ -14,3 +30,4 @@ pwd
 sleep 1;
 echo " Now we will play the game..."
 ./game.sh
+fi
